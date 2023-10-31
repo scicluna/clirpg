ecb08f891aa98896084a37c7f7495be5877a8ac2
scicluna <88948700+scicluna@users.noreply.github.com>
2023-10-31 16:21:07 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    # Game initialization logic here
+    
+    while True:
+        # Main game loop
+        action = input("What would you like to do? ")
+        
+        if action == "quit":
+            break
+        # Handle other actions here
+
+if __name__ == "__main__":
+    main()
